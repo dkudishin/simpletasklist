ffe32e7a3abc8aada80902719daa2887c14c8a9d
dkudishin <dkudishin@gmail.com>
2023-08-05 21:29:37 +0200

    add Mockito tests

---
+++ src/test/java/dk/kudishin/simpletasklist/services/TaskProcessorSpringTest.java
@@ -0,0 +1,39 @@
+package dk.kudishin.simpletasklist.services;
+
+import dk.kudishin.simpletasklist.data.TaskDao;
+import dk.kudishin.simpletasklist.domain.Task;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+/*
+Things to understand:
+ - this is a SpringBootTest
+ - the value it brings is 0. It's only purpose in life is to demonstrate Mockito
+ */
+@SpringBootTest
+class TaskProcessorSpringTest {
+
+    @Autowired
+    private TaskProcessor taskProcessor;
+
+    @MockBean
+    @Qualifier
+    private TaskDao taskDao;
+
+    @Test
+    void getAllTasks() {
+        when(taskDao.getAllTasks())
+                .thenReturn(Collections.emptyList());
+        List<Task> allTasks = taskProcessor.getAllTasks();
+        assertEquals(0, allTasks.size());
+    }
+}
---
+++ src/test/java/dk/kudishin/simpletasklist/services/TaskProcessorTest.java
@@ -0,0 +1,31 @@
+package dk.kudishin.simpletasklist.services;
+
+import dk.kudishin.simpletasklist.data.TaskDao;
+import dk.kudishin.simpletasklist.domain.Task;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/*
+Things to understand:
+ - this is NOT a SpringBootTest
+ - the value it brings is 0. It's only purpose in life is to demonstrate Mockito
+ */
+class TaskProcessorTest {
+
+    private final TaskDao taskDao = mock(TaskDao.class);
+
+    @Test
+    void getAllTasks() {
+        TaskProcessor taskProcessor = new TaskProcessor(taskDao);
+        when(taskDao.getAllTasks())
+                .thenReturn(Collections.emptyList());
+        List<Task> allTasks = taskProcessor.getAllTasks();
+        assertEquals(0, allTasks.size());
+    }
+}
